3c903ff3d85a95dac5fb00f6e3a95233591db0e4
BrightLeviBennett <bright.bennett@icloud.com>
2025-12-09 11:50:12 -0800

    fix animations again cuz the push/pull is stupid and got rid of my changes

--- scenes/Crater.gd
+++ scenes/Crater.gd
@@ -1,52 +1,233 @@
-extends Node2D
+[gd_scene load_steps=14 format=3 uid="uid://cwscv5rsr5tom"]
 
-# Tries %Player first (unique name), then searches by group "player"
-@onready var player: Node = get_node_or_null("%Player")
+[ext_resource type="Script" uid="uid://bog38pjfcm0ni" path="res://scenes/Crater.gd" id="1_x3en3"]
+[ext_resource type="Texture2D" uid="uid://b8tc4jco4iik4" path="res://art/Crater.png" id="2_cedul"]
+[ext_resource type="Script" uid="uid://b88jsd4oooxk2" path="res://addons/daicon/nodes/daicon_node.gd" id="2_hur4u"]
+[ext_resource type="Script" uid="uid://dxf4xy7bwjwgp" path="res://scripts/Player.gd" id="3_hur4u"]
+[ext_resource type="Texture2D" uid="uid://cmqglxb811emk" path="res://art/Robot.png" id="3_yf6uk"]
+[ext_resource type="PackedScene" uid="uid://c8btfiwna3ca" path="res://scenes/Spot.tscn" id="5_yf6uk"]
+[ext_resource type="Texture2D" uid="uid://bf4acfsvv6cwg" path="res://icon.svg" id="6_okvjp"]
 
-func _ready():
-	# If player still null, try a tolerant search
-	if player == null:
-		var found := get_tree().get_nodes_in_group("player")
-		if found.size() > 0:
-			player = found[0]
-			print("[Room] player:", player)
+[sub_resource type="NavigationPolygon" id="NavigationPolygon_yf6uk"]
+vertices = PackedVector2Array(2347, 1416.0078, -477.96875, 1420.9766, -476.46094, 786.71875, 2347.5469, 786.71875)
+polygons = Array[PackedInt32Array]([PackedInt32Array(0, 1, 2, 3)])
+outlines = Array[PackedVector2Array]([PackedVector2Array(-486.44092, 776.7167, 2357.559, 776.7167, 2357, 1426, -488, 1431)])
 
-	# Collect all click spots anywhere in the tree (group-based, not path-based)
-	var spots := get_tree().get_nodes_in_group("move_spot")
-	print("[Room] found spots:", spots.size())
+[sub_resource type="Animation" id="Animation_okvjp"]
+length = 0.001
+tracks/0/type = "value"
+tracks/0/imported = false
+tracks/0/enabled = true
+tracks/0/path = NodePath("Path2D/PathFollow2D:position")
+tracks/0/interp = 1
+tracks/0/loop_wrap = true
+tracks/0/keys = {
+"times": PackedFloat32Array(0),
+"transitions": PackedFloat32Array(1),
+"update": 0,
+"values": [Vector2(352.94708, 1062.4722)]
+}
+tracks/1/type = "value"
+tracks/1/imported = false
+tracks/1/enabled = true
+tracks/1/path = NodePath("Path2D/PathFollow2D:rotation")
+tracks/1/interp = 1
+tracks/1/loop_wrap = true
+tracks/1/keys = {
+"times": PackedFloat32Array(0),
+"transitions": PackedFloat32Array(1),
+"update": 0,
+"values": [1.5690475]
+}
+tracks/2/type = "value"
+tracks/2/imported = false
+tracks/2/enabled = true
+tracks/2/path = NodePath("Path2D2/PathFollow2D:position")
+tracks/2/interp = 1
+tracks/2/loop_wrap = true
+tracks/2/keys = {
+"times": PackedFloat32Array(0),
+"transitions": PackedFloat32Array(1),
+"update": 0,
+"values": [Vector2(-454, 0)]
+}
+tracks/3/type = "value"
+tracks/3/imported = false
+tracks/3/enabled = true
+tracks/3/path = NodePath("Path2D2/PathFollow2D:rotation")
+tracks/3/interp = 1
+tracks/3/loop_wrap = true
+tracks/3/keys = {
+"times": PackedFloat32Array(0),
+"transitions": PackedFloat32Array(1),
+"update": 0,
+"values": [0.0]
+}
 
-	for s in spots:
-		if s.has_signal("selected") and not s.is_connected("selected", Callable(self, "_on_spot_selected")):
-			s.connect("selected", Callable(self, "_on_spot_selected"))
-			print("[Room] connected spot:", s.name, "@", s.global_position)
+[sub_resource type="Animation" id="Animation_pquwk"]
+resource_name = "intro"
+length = 10.5
+step = 55.0
+tracks/0/type = "value"
+tracks/0/imported = false
+tracks/0/enabled = true
+tracks/0/path = NodePath("Path2D/PathFollow2D:position")
+tracks/0/interp = 1
+tracks/0/loop_wrap = true
+tracks/0/keys = {
+"times": PackedFloat32Array(0, 3, 4.5, 5.8, 6),
+"transitions": PackedFloat32Array(1, 1, 1, 1, 1),
+"update": 0,
+"values": [Vector2(-240, 0), Vector2(385.02106, -1.8542477), Vector2(352.87338, -108.21162), Vector2(357.45627, 995.0726), Vector2(285.00525, 1081.7716)]
+}
+tracks/1/type = "value"
+tracks/1/imported = false
+tracks/1/enabled = true
+tracks/1/path = NodePath("Path2D/PathFollow2D:rotation")
+tracks/1/interp = 1
+tracks/1/loop_wrap = true
+tracks/1/keys = {
+"times": PackedFloat32Array(0, 3, 3.0933, 4.5, 5.8, 6),
+"transitions": PackedFloat32Array(1, 1, 1, 1, 1, 1),
+"update": 0,
+"values": [0.09599310885968812, 0.09599310885968812, -0.09599310885968812, -0.09599310885968812, -0.17453292519943295, -1.5707963267948966]
+}
+tracks/2/type = "value"
+tracks/2/imported = false
+tracks/2/enabled = true
+tracks/2/path = NodePath("Path2D2/PathFollow2D:position")
+tracks/2/interp = 1
+tracks/2/loop_wrap = true
+tracks/2/keys = {
+"times": PackedFloat32Array(0, 2.7, 4.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 10),
+"transitions": PackedFloat32Array(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+"update": 0,
+"values": [Vector2(-454, 0), Vector2(995.23956, -0.011459317), Vector2(1097.8872, -109.657104), Vector2(1118.5916, 727.5547), Vector2(1114.3322, 738.36163), Vector2(1097.6145, 716.7983), Vector2(1074.6727, 702.632), Vector2(1052.4075, 714.50916), Vector2(1043.0011, 740.178), Vector2(1035.9519, 766.5153), Vector2(1035.0018, 848.84753)]
+}
+tracks/3/type = "value"
+tracks/3/imported = false
+tracks/3/enabled = true
+tracks/3/path = NodePath("Path2D2/PathFollow2D:rotation")
+tracks/3/interp = 1
+tracks/3/loop_wrap = true
+tracks/3/keys = {
+"times": PackedFloat32Array(0, 2.7, 4.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 10),
+"transitions": PackedFloat32Array(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+"update": 0,
+"values": [0.09599310885968812, 0.0, -0.83699554, 1.5486554, -2.1193457, -2.3876193, -2.925852, 2.010639, 1.8457062, 1.7236581, 1.5827005]
+}
 
-func _on_spot_selected(spot: Node) -> void:
-	print("[Room] spot selected:", spot.name, "->", spot.global_position)
-	if player and player.has_method("set_move_target"):
-		player.call("set_move_target", spot.global_position)
-	else:
-		push_error("[Room] Player missing or set_move_target() not found")
+[sub_resource type="AnimationLibrary" id="AnimationLibrary_8a84o"]
+_data = {
+&"RESET": SubResource("Animation_okvjp"),
+&"intro": SubResource("Animation_pquwk")
+}
 
-func _on_hotspot_clicked(h):
-	# If you want: optionally move player close before interacting.
-	# Simple rule: if item required, check it:
-	if h.requires_item != "" and not GameState.has_item(h.requires_item):
-		_say(Dialogue.get_text(h.dialogue_key + "_need"))
-		return
+[sub_resource type="Curve2D" id="Curve2D_okvjp"]
+_data = {
+"points": PackedVector2Array(0, 0, 0, 0, -240, 0, 0, 0, 0, 0, 385.88855, 1.0157479, 0, 0, 0, 0, 350.88818, -114.77951, 0, 0, 0, 0, 357.4765, 998.48016, 0, 0, 0, 0, 285.0052, 1081.7715)
+}
+point_count = 5
 
-	if h.give_item != "":
-		GameState.add_item(h.give_item)
-		_say(Dialogue.get_text(h.dialogue_key + "_got"))
-	elif h.flag_to_set != "":
-		GameState.set_flag(h.flag_to_set, true)
-		_say(Dialogue.get_text(h.dialogue_key + "_done"))
-	else:
-		_say(Dialogue.get_text(h.dialogue_key))
+[sub_resource type="Curve2D" id="Curve2D_pquwk"]
+_data = {
+"points": PackedVector2Array(0, 0, 0, 0, -454, 0, 0, 0, 0, 0, 999, 0, 0, 0, 0, 0, 1100, -112, 0, 0, 0, 0, 1119, 746, 0, 0, 0, 0, 1108, 728, 0, 0, 0, 0, 1099, 718, 0, 0, 0, 0, 1085, 706, 0, 0, 0, 0, 1077, 703, 0, 0, 0, 0, 1070, 702, 0, 0, 0, 0, 1062, 705, 0, 0, 0, 0, 1055, 709, 0, 0, 0, 0, 1047, 726, 0, 0, 0, 0, 1036, 765, 0, 0, 0, 0, 1035, 849)
+}
+point_count = 14
 
-@onready var ui_dialogue: Label = get_node_or_null("UI/DialogueLabel")
+[node name="Crater" type="Node2D"]
+script = ExtResource("1_x3en3")
+metadata/_edit_horizontal_guides_ = [1122.0]
 
-func _say(text: String) -> void:
-	ui_dialogue.text = text
-	ui_dialogue.visible = true
-	ui_dialogue.modulate.a = 1.0
-	ui_dialogue.create_tween().tween_property(ui_dialogue, "modulate:a", 0.0, 2.0)
+[node name="UI" type="CanvasLayer" parent="."]
+
+[node name="DialogueLabel" type="Label" parent="UI"]
+anchors_preset = -1
+anchor_left = 0.25800002
+anchor_top = -0.72985494
+anchor_right = 0.47000003
+anchor_bottom = -0.42900002
+offset_left = -0.9191284
+offset_top = 0.24334717
+offset_right = -0.4000244
+offset_bottom = 0.32000732
+metadata/_edit_use_anchors_ = true
+
+[node name="Layer_Far" type="Node2D" parent="."]
+z_index = -100
+y_sort_enabled = true
+script = ExtResource("2_hur4u")
+metadata/_custom_type_script = "uid://b88jsd4oooxk2"
+
+[node name="Background" type="Sprite2D" parent="Layer_Far"]
+position = Vector2(1003.4408, 470.28326)
+scale = Vector2(28.408815, 19.285666)
+texture = ExtResource("2_cedul")
+
+[node name="Layer_Mid" type="Node2D" parent="."]
+y_sort_enabled = true
+script = ExtResource("2_hur4u")
+metadata/_custom_type_script = "uid://b88jsd4oooxk2"
+
+[node name="NavRegion" type="NavigationRegion2D" parent="Layer_Mid"]
+position = Vector2(66, -337)
+navigation_polygon = SubResource("NavigationPolygon_yf6uk")
+
+[node name="Props" type="Node2D" parent="Layer_Mid"]
+position = Vector2(155.44092, -351.7167)
+
+[node name="Spots" type="Node2D" parent="Layer_Mid"]
+
+[node name="Spot" parent="Layer_Mid/Spots" instance=ExtResource("5_yf6uk")]
+position = Vector2(1037, 847)
+
+[node name="Spot2" parent="Layer_Mid/Spots" instance=ExtResource("5_yf6uk")]
+position = Vector2(99, 857)
+
+[node name="AnimationPlayer" type="AnimationPlayer" parent="."]
+libraries = {
+&"": SubResource("AnimationLibrary_8a84o")
+}
+autoplay = "intro"
+
+[node name="Path2D" type="Path2D" parent="."]
+position = Vector2(-23.14862, 0)
+scale = Vector2(2.4285474, 0.9844962)
+curve = SubResource("Curve2D_okvjp")
+
+[node name="PathFollow2D" type="PathFollow2D" parent="Path2D"]
+position = Vector2(352.94708, 1062.4722)
+rotation = 1.5690475
+progress = 1970.5409
+
+[node name="josef" type="CharacterBody2D" parent="Path2D/PathFollow2D"]
+unique_name_in_owner = true
+rotation = 0.0052879085
+scale = Vector2(0.39603958, 0.99999994)
+motion_mode = 1
+script = ExtResource("3_hur4u")
+
+[node name="Sprite2D" type="Sprite2D" parent="Path2D/PathFollow2D/josef"]
+scale = Vector2(2.28, 2.05)
+texture = ExtResource("3_yf6uk")
+
+[node name="NavigationAgent2D" type="NavigationAgent2D" parent="Path2D/PathFollow2D/josef"]
+
+[node name="Path2D2" type="Path2D" parent="."]
+curve = SubResource("Curve2D_pquwk")
+
+[node name="PathFollow2D" type="PathFollow2D" parent="Path2D2"]
+position = Vector2(-454, 0)
+progress = 2690.283
+
+[node name="berta" type="CharacterBody2D" parent="Path2D2/PathFollow2D"]
+unique_name_in_owner = true
+motion_mode = 1
+script = ExtResource("3_hur4u")
+
+[node name="Sprite2D" type="Sprite2D" parent="Path2D2/PathFollow2D/berta"]
+position = Vector2(0, 8.122079)
+scale = Vector2(0.576, 1.173)
+texture = ExtResource("6_okvjp")
+
+[node name="NavigationAgent2D" type="NavigationAgent2D" parent="Path2D2/PathFollow2D/berta"]
